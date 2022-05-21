499900b509b5e0a344a341f347bed388f3cb2d74
3vilguy <2122619+3vilguy@users.noreply.github.com>
2022-05-21 17:31:46 +0100

    Update description, add help and args validation.

--- cmd/root.go
+++ cmd/root.go
@@ -1,24 +1,38 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "numbers-to-words",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:                   "numbers-to-words [number]",
+	Short:                 "Convert number to English words",
+	Long:                  "Takes a number, from 0 to 100000, and converts the number into grammatically correct English words.",
+	DisableFlagsInUseLine: true,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New("requires exactly one argument\n")
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		val, err := strconv.Atoi(args[0])
+		if err != nil {
+			return errors.New(fmt.Sprintf("%s is not a number\n", args[0]))
+		}
+		if val < 0 || val > 100000 {
+			return errors.New(fmt.Sprintf("%s has to be a number from 0 to 100000\n", args[0]))
+		}
+
+		return nil
+	},
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -39,5 +53,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
